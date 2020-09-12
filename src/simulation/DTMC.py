304d0b87898e0865adb62badfdbb08266eb72b25
Mathew Jacob <63362973+mjacob1002@users.noreply.github.com>
2020-09-12 10:26:01 -0400

    Update DTMC simulation file
    Added SIRS modeling function

--- src/simulation/DTMC.py
+++ src/simulation/DTMC.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # tested
-def dtmc_sir(beta: float, mu_ir: float, S0: int, I0: int, R0: int, time: int, xlabel: str):
+def dtmc_sir(beta: float, mu_ir: float, S0: int, I0: int, R0: int, time: int, xlabel: str, plot = True):
     """
     Uses a Discrete Time Markov Chain method in order to plot a standard SIR model.
     :param beta: effective transmission rate
@@ -13,7 +13,8 @@ def dtmc_sir(beta: float, mu_ir: float, S0: int, I0: int, R0: int, time: int, xl
     :param R0: initial amount of removeds
     :param time: duration of the markov chain; how many unit times
     :param xlabel: the unit of time for the "time" variable
-    :return: nothing for now
+    :param plot: if true, the method will display the plot. Otherwise, it just returns the arrays
+    :return: S, I , R, t arrays
     """
     n = S0 + I0 + R0
     S, I, R, t = __initSIR__(S0, I0, R0, time)
@@ -21,19 +22,20 @@ def dtmc_sir(beta: float, mu_ir: float, S0: int, I0: int, R0: int, time: int, xl
     for i in range(1, time + 1):
         p = beta * I[i - 1] / n
         print("P: ", p, " I: ", I[i - 1])
-        s_i = np.random.binomial(n=int(S[i - 1]), p=p, size=1)
-        i_r = np.random.binomial(n=int(I[i - 1]), p=mu_ir, size=1)
+        s_i = np.random.binomial(n=int(S[i - 1]), p=p, size=1000).mean()
+        i_r = np.random.binomial(n=int(I[i - 1]), p=mu_ir, size=1000).mean()
         S[i] = S[i - 1] - s_i
         I[i] = I[i - 1] + s_i - i_r
         R[i] = R[i - 1] + i_r
-    plt.plot(t, I, label="Infected")
-    plt.plot(t, S, label="Susceptible")
-    plt.plot(t, R, label="Removed")
-    plt.xlabel(xlabel=xlabel)
-    plt.ylabel("Number of People")
-    plt.legend()
-    plt.title()
-    plt.show()
+    if plot == True:
+        plt.plot(t, I, label="Infected")
+        plt.plot(t, S, label="Susceptible")
+        plt.plot(t, R, label="Removed")
+        plt.xlabel(xlabel=xlabel)
+        plt.ylabel("Number of People")
+        plt.legend()
+        plt.title()
+        plt.show()
     return S, I, R, t
 
 
@@ -61,7 +63,7 @@ def __initSIRV__(S0: int, I0: int, R0: int, V0: int, t: int):
 
 
 # tested
-def dtmc_sirv(t: int, beta: float, mu_ir: float, mu_sv: float, S0: int, I0: int, R0: int, V0 = 0.0, x_label = "Days"):
+def dtmc_sirv(t: int, beta: float, mu_ir: float, mu_sv: float, S0: int, I0: int, R0: int, V0 = 0.0, x_label = "Days", plot = True):
     """
     Susceptible, Infected, Removed, Vaccinated compartmental model
     :param beta: effective transmission rate
@@ -73,6 +75,7 @@ def dtmc_sirv(t: int, beta: float, mu_ir: float, mu_sv: float, S0: int, I0: int,
     :param V0: Starting amount of Vaccinated people at the beginning of the simulation
     :param t: The amount of time the simulation should last
     :param x_label: The unit time used i.e hours, days, etc.
+    :param plot: if true, display the plot of all arrays. Otherwise it will just return arrays
     :return: Susceptibles, Infected, Removed, Vaccinated arrays with values at each state change, as well as array of differnet time intervals
 
     """
@@ -80,30 +83,31 @@ def dtmc_sirv(t: int, beta: float, mu_ir: float, mu_sv: float, S0: int, I0: int,
     S, I, R, V, t_vec = __initSIRV__(S0, I0, R0, V0, t)
     for i in range(1, t + 1):
         p = beta * I[i - 1] / n
-        s_i = np.random.binomial(n=S[i - 1], p=p, size=1)
-        i_r = np.random.binomial(n=I[i - 1], p=mu_ir, size=1)
+        s_i = np.random.binomial(n=S[i - 1], p=p, size=1000).mean()
+        i_r = np.random.binomial(n=I[i - 1], p=mu_ir, size=1000).mean()
         Stemp = int(S[i - 1] - s_i)
-        s_v = np.random.binomial(n=Stemp, p=mu_sv, size=1)
+        s_v = np.random.binomial(n=Stemp, p=mu_sv, size=1000).mean()
         S[i] = S[i - 1] - s_i - s_v
         I[i] = I[i - 1] + s_i - i_r
         R[i] = R[i - 1] + i_r
         V[i] = V[i - 1] + s_v
         print("STemp: ", Stemp, " S: ", S[i - 1], " I: ", I[i - 1], "R: ", R[i - 1], "V: ", V[i - 1])
     title = "SIRV Model; Beta = " + str(beta) + " Gamma= " + str(mu_ir) + "vaccine rate = " + str(mu_sv)
-    plt.plot(t_vec, I, label="Infected")
-    plt.plot(t_vec, S, label="Susceptible")
-    plt.plot(t_vec, R, label="Removed")
-    plt.plot(t_vec, V, label="Vaccine")
-    plt.xlabel(xlabel=x_label)
-    plt.ylabel("Number of People")
-    plt.title(title)
-    plt.legend()
-    plt.show()
+    if plot == True:
+        plt.plot(t_vec, I, label="Infected")
+        plt.plot(t_vec, S, label="Susceptible")
+        plt.plot(t_vec, R, label="Removed")
+        plt.plot(t_vec, V, label="Vaccine")
+        plt.xlabel(xlabel=x_label)
+        plt.ylabel("Number of People")
+        plt.title(title)
+        plt.legend()
+        plt.show()
     return S, I, R, V, t_vec
 
 
 # tested
-def dtmc_sis(beta: float, mu_is: float, S0: int, I0: int, t: int, x_label: str):
+def dtmc_sis(beta: float, mu_is: float, S0: int, I0: int, t: int, x_label: str, plot = True):
     """
     Susceptible Infected Susceptible Compartmental Model
     :param beta: effective transmission rate per unit time t
@@ -127,22 +131,23 @@ def dtmc_sis(beta: float, mu_is: float, S0: int, I0: int, t: int, x_label: str):
     n = S0 + I0
     for i in range(1, t + 1):
         p = beta * I[i - 1] / n
-        s_i = np.random.binomial(S[i - 1], p, 1)
-        i_s = np.random.binomial(I[i - 1], mu_is, 1)
+        s_i = np.random.binomial(S[i - 1], p, 1000).mean()
+        i_s = np.random.binomial(I[i - 1], mu_is, 1000).mean()
         S[i] = S[i - 1] - s_i + i_s
         I[i] = I[i - 1] + s_i - i_s
         print("S; ", S[i - 1], " I: ", I[i - 1])
-    plt.plot(t_vec, S, label="Susceptible")
-    plt.plot(t_vec, I, label="Infected")
-    plt.xlabel(x_label)
-    plt.ylabel("Number of People")
-    plt.legend()
-    plt.show()
+    if plot == True:
+        plt.plot(t_vec, S, label="Susceptible")
+        plt.plot(t_vec, I, label="Infected")
+        plt.xlabel(x_label)
+        plt.ylabel("Number of People")
+        plt.legend()
+        plt.show()
     return S, I, t_vec
 
 
 # tested
-def seir(beta: float, mu_ei: float, mu_ir: float, S0: int, E0: int, I0: int, R0: int, time: int, x_label: str):
+def seir(beta: float, mu_ei: float, mu_ir: float, S0: int, E0: int, I0: int, R0: int, time: int, x_label: str, plot = True):
     """
     Susceptible, Exposed, Infected, Removed compartmental model. Assumes that exposed individuals cannot transmit the
     disease.
@@ -172,31 +177,51 @@ def seir(beta: float, mu_ei: float, mu_ir: float, S0: int, E0: int, I0: int, R0:
     S, E, I, R, t_vec = __initSEIR__(S0, E0, I0, R0, time)
     for i in range(1, time + 1):
         p = beta * I[i - 1] / n
-        s_e = np.random.binomial(S[i - 1], p, 1)
-        e_i = np.random.binomial(E[i - 1], mu_ei, 1)
-        i_r = np.random.binomial(I[i - 1], mu_ir, 1)
+        s_e = np.random.binomial(S[i - 1], p, 1000).mean()
+        e_i = np.random.binomial(E[i - 1], mu_ei, 1000).mean()
+        i_r = np.random.binomial(I[i - 1], mu_ir, 1000).mean()
         S[i] = S[i - 1] - s_e
         E[i] = E[i - 1] + s_e - e_i
         I[i] = I[i - 1] + e_i - i_r
         R[i] = R[i - 1] + i_r
-    plt.plot(t_vec, S, label="Susceptible")
-    plt.plot(t_vec, I, label="Infected")
-    plt.plot(t_vec, E, label="Exposed")
-    plt.plot(t_vec, R, label="Removed")
-    plt.xlabel = x_label
-    plt.ylabel = "Number of People"
-    plt.legend()
-    plt.show()
+    if plot == True:
+        plt.plot(t_vec, S, label="Susceptible")
+        plt.plot(t_vec, I, label="Infected")
+        plt.plot(t_vec, E, label="Exposed")
+        plt.plot(t_vec, R, label="Removed")
+        plt.xlabel = x_label
+        plt.ylabel = "Number of People"
+        plt.legend()
+        plt.show()
     return S, E, I, R, t_vec
 
 
-def sirs(beta: float, mu_ir: float, mu_rs: float, S0: int, I0: int, R0: int, time:int, xlabel = "Hours"):
+def sirs(beta: float, mu_ir: float, mu_rs: float, S0: int, I0: int, R0: int, time:int, xlabel = "Hours", title = "SIRS", fname = "SIRSfig.png",plot = True):
+    def __init_sirs__(S0: int, I0: int, R0: int, time: int):
+        S, I, R, t = np.zeros(time+1), np.zeros(time+1), np.zeros(time+1), np.zeros(time+1)
+        S[0], I[0], R[0] = S0, I0, R0
+        for i in range(t+1):
+            t[i] = i
+        return S, I, R, t
     n = S0 + I0 + R0
-    num_of_entries = time + 1
-    S, I, R = np.zeros(num_of_entries), np.zeros(num_of_entries), np.zeros(num_of_entries)
-    S[0], I[0], R[0] = S0, I0, R0
-    
-    
+    S, I, R, t = __init_sirs__(S0, I0, R0, time)
+    for i in range(1, time+1):
+        p = beta * I[i-1] / n
+        s_i = np.random.binomial(S[i-1], p, 1000).mean()
+        i_r = np.random.binomial(I[i-1], mu_ir, 1000).mean()
+        r_s = np.random.binomial(R[i-1], mu_rs, 1000).mean()
+        S[i] = S[i-1] - s_i + r_s
+        I[i] = I[i-1] + s_i - i_r
+        R[i] = R[i-1] + i_r - r_s
+    if plot == True:
+        plt.plot(t, S, "Susceptibles")
+        plt.plot(t, I, "Infected")
+        plt.plot(t, R, "Removed")
+        plt.xlabel = x_label
+        plt.ylabel = "Number of People"
+        plt.legend()
+        plt.show()
+    return S, I, R, t
 # test:
 
 beta1 = 2 / 24
@@ -209,4 +234,7 @@ R0 = 0
 time = 30 * 24
 xlabel = "Hours"
 #seir(beta=beta1, mu_ei=ei, mu_ir=ir, S0=S0, E0=E0, I0=I0, R0=R0, time=time, x_label=xlabel)
-dtmc_sir(beta=beta1, mu_ir=ir, S0=S0, I0=I0, R0=R0, time=time, xlabel="Hours")
+#dtmc_sir(beta=beta1, mu_ir=ir, S0=S0, I0=I0, R0=R0, time=time, xlabel="Hours")
+
+a = np.random.binomial(100, 0.05, 1000).mean()
+print(a)
