f39be4a76ac6180f255e273dd6bc47435b94d746
Mathew Jacob <mjacob1002@gmail.com>
2021-03-18 09:28:11 -0400

    Debugged errors involving the inheritane doubling the number of elements and thus causing an out of bounds error.

--- src/DTMC/spatialModel/Hub/HubSIR.py
+++ src/DTMC/spatialModel/Hub/HubSIR.py
@@ -1,10 +1,14 @@
-from .HubSIS import HubSIS
+
 from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
+
+from .HubSIS import HubSIS
 from src.utility import Person
 import src.utility as u
 from multipledispatch import dispatch
+from src.DTMC.spatialModel.simul_details import Simul_Details
+
 
 
 class HubSIR(HubSIS):
@@ -12,12 +16,18 @@ class HubSIR(HubSIS):
                  days: int,
                  gamma: float, w0=1.0,
                  hubConstant=6 ** 0.5):
+        # initialize the Simul_Details object
+        self.details = Simul_Details(days=days, popsize=popsize)
+
         self.R0 = R0
         super(HubSIR, self).__init__(popsize, pss, rstart, alpha, side, S0, I0, days, gamma, w0, hubConstant)
         # create and initialize the removed array
         self.R = np.zeros(days + 1)
         self.R[0] = R0
-        # create the R collection data structure
+        # create the R collection data structure and set the array structures back to undefined array
+        # to undo the effects of the inheritance
+        self.Scollect = []
+        self.Icollect = []
         self.Rcollect = []
 
         # initialize the Scollect and Icollect arrays
@@ -31,6 +41,8 @@ class HubSIR(HubSIS):
             self.Scollect.append(p1)
             self.Icollect.append(p2)
             self.Rcollect.append(p3)
+            self.details.addLocation(0, (self.locx[i], self.locy[i]))
+
         # this loop will make the isIncluded = True for all the infecteds
         for i in range(S0, S0 + I0):
             # create the two person objects, with everything identical except the isIncluded boolean
@@ -41,6 +53,7 @@ class HubSIR(HubSIS):
             self.Scollect.append(p1)
             self.Icollect.append(p2)
             self.Rcollect.append(p3)
+            self.details.addLocation(0, (self.locx[i], self.locy[i]))
         # initialize the Rcollect array
         for i in range(S0 + I0, S0 + I0 + R0):
             # create the two person objects, with everything identical except the isIncluded boolean
@@ -51,29 +64,46 @@ class HubSIR(HubSIS):
             self.Scollect.append(p1)
             self.Icollect.append(p2)
             self.Rcollect.append(p3)
-
+            self.details.addLocation(0, (self.locx[i], self.locy[i]))
+            self.details.addStateChange(i, "R", 0)
+        print("Initial S0: ", self.S[0], " Initial I0: ", self.I[0], " Initial R0: ", self.R[0])
     # run state changes from I to R
     def _ItoR(self):
-        # call the super method __ItoS, as it is effectively the same thing.
-        # only difference will be that the transfers set will go from I to R
-        transfers = self.__ItoS()
+        # set that keeps track of the indices of people that changed states
+        transfers = set()
+        for count, inf in enumerate(self.Icollect):
+            if not inf.isIncluded:
+                continue
+            #print("infected person ", count)
+            event = u.randEvent(self.gamma)
+            if not event:
+                continue
+            self.Icollect[count].isIncluded = False
+            transfers.add(count)
         return transfers
 
     # run the simulation using
-    def run(self):
+    def run(self, getDetails=True):
         for i in range(1, self.days + 1):
+            print("Day ",i)
             # run the transfers from different compartments
-            transferSI = self._StoI()
+            transferSI = self._StoI(i)
             transferIr = self._ItoR()
             # go after and change the indices in the collection data structure thing
             for index in transferSI:
+                #print("Person ", index)
                 self.Icollect[index].isIncluded = True
+                self.details.addStateChange(index, "I", i)
             for index in transferIr:
-                self.Scollect[index].isIncluded = True
+                #print("Person ", index)
+                self.Rcollect[index].isIncluded = True
+                self.details.addStateChange(index, "R", i)
             # change the number of people in each state on the day i by adjusting the previous day's count
             self.S[i] = self.S[i - 1] - len(transferSI)
             self.I[i] = self.I[i - 1] + len(transferSI) - len(transferIr)
             self.R[i] = self.R[i-1] + len(transferIr)
+        if getDetails:
+            return self.details
 
     # maybe add picking what to plot later
     def plot(self):
@@ -81,7 +111,7 @@ class HubSIR(HubSIS):
         fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, sharex='all')
         ax1.plot(t, self.S, label="Susceptible", color='r')
         ax1.set_ylabel("Number of Susceptible People")
-        ax1.set_title("Hub SIS Simulation")
+        ax1.set_title("Hub SIR Simulation")
         ax2.plot(t, self.I, label="Active Cases", color='b')
         ax2.set_ylabel("Active Cases")
         ax3.plot(t, self.R, label="Recovered", color='m')
