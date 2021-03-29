a753cbad463ba9228cd4d51377df0af0a1026a8f
Mathew Jacob <mjacob1002@gmail.com>
2021-03-28 22:51:35 -0400

    Finished randMovementSIR class and fixed inheritance stuff with the randMoveSIS model.

--- src/DTMC/spatialModel/randomMovement/randMoveSIR.py
+++ src/DTMC/spatialModel/randomMovement/randMoveSIR.py
@@ -7,10 +7,9 @@ import src.utility as u
 from src.DTMC.spatialModel.simul_details import Simul_Details
 # not to be confused with the person object that is used in the Hub/Strong Infectious Model
 from src.utility import Person1 as Person
-from .randMove import RandMove
+from src.DTMC.spatialModel.randomMovement.randMoveSIS import RandMoveSIS
 
-
-class RandMoveSIS(RandMove):
+class RandMoveSIR(RandMoveSIS):
     """
     An SIS model that follows the Random Movement Model. When the individuals in the simulation move, 
     they move according to a randomly generated angle and a randomly generated radius.
@@ -23,9 +22,12 @@ class RandMoveSIS(RandMove):
     
     I0: int
         The starting number of infectious individuals in the simulation. 
+    
+    R0: int
+        The starting number of recovered individuals in the simulation.
 
     gamma: float
-        The recovery probability of an individual going from I -> S
+        The recovery probability of an individual going from I -> R.
     
     planeSize : float
         The length of each side of the square plane in which the individuals are confined to. For example,
@@ -55,7 +57,7 @@ class RandMoveSIS(RandMove):
     days: int
         The number of days that was simulated.
     
-    W0: float optional
+    w0: float optional
         The probability of infection if the distance between an infectious person and susceptible person is 0.
     
     alpha: float optional
@@ -75,12 +77,17 @@ class RandMoveSIS(RandMove):
         
     Scollect: list
         Used to keep track of the states each Person object is in. If the copy of a Person object has 
-        isIncluded == True, then the person is susceptible. Has a total of popsize Person objects,
+        isIncluded == True, then the person is SUSCEPTIBLE. Has a total of popsize Person objects,
         with numbers [0, popsize). 
     
     Icollect: list
          Used to keep track of the states each Person object is in. If the copy of a Person object has 
         isIncluded == True, then the person is INFECTED. Has a total of popsize Person objects,
+        with numbers [0, popsize).
+    
+    Rcollect: list
+        Used to keep track of the states each Person object is in. If the copy of a Person object has 
+        isIncluded == True, then the person is RECOVERED. Has a total of popsize Person objects,
         with numbers [0, popsize).
 
 
@@ -91,28 +98,19 @@ class RandMoveSIS(RandMove):
     
      """
 
-    def __init__(self, S0:int, I0:int, gamma:float, planeSize:float, move_r:float, sigma_R:float, spread_r:float, sigma_r: float,
+    def __init__(self, S0:int, I0:int, R0:int, gamma:float, planeSize:float, move_r:float, sigma_R:float, spread_r:float, sigma_r: float,
     days:int, w0=1.0, alpha=2.0):
-        # call to super constructor
-        super(RandMoveSIS, self).__init__(planeSize, move_r, spread_r, w0=w0)
-        self.details = Simul_Details(days=days, popsize=S0+I0)
-        # standard deviation of movement radius
-        self.sigma_R = sigma_R
-        # total population size
-        self.popsize = S0 + I0
-        # get the days
-        self.days = days
-        # initialize the gamma value
-        self.gamma = gamma
-        # generate the data structure with the arrays
-        self.S, self.I = np.zeros(days+1), np.zeros(days+1)
-        # initialize day 0 to have the starting susceptibles and infecteds
-        self.S[0], self.I[0] = S0, I0
-        # generate the special collections that hold the Person objects
-        self.Scollect = []
-        self.Icollect = []
+        super(RandMoveSIR, self).__init__(S0=S0, I0=I0, gamma=gamma, planeSize=planeSize, move_r=move_r, sigma_R=sigma_R, spread_r=spread_r, sigma_r=sigma_r,
+        days=days, w0=w0, alpha=alpha)
+        self.R0 = R0
+        self.R = np.zeros(days+1)
+        self.R[0] = R0
+        self.popsize += self.R0
+        # redirect details object to point to another object to delete copies in inheritance
+        self.details = Simul_Details(days, self.popsize)
+        self.Scollect, self.Icollect, self.Rcollect = [], [], []
         spreading_r = np.random.normal(spread_r, sigma_r, S0+I0)
-        # generate the random x, y locations
+        # generate the random x, y locations with every position within the plane being equally likely
         loc_x = np.random.random(S0+I0) * planeSize
         loc_y = np.random.random(S0+I0) * planeSize
         # create the special objects:
@@ -123,6 +121,7 @@ class RandMoveSIS(RandMove):
             # periodic mobility model
             p1 = Person(loc_x[i], loc_y[i], 0, spreading_r[i])
             p2 = Person(loc_x[i], loc_y[i], 0, spreading_r[i]) 
+            p3 = Person(loc_x[i], loc_y[i], 0, spreading_r[i])
             self.details.addLocation(0, (loc_x[i], loc_y[i]))       
             # if the person is in the susceptible objects created
             if i < S0:
@@ -131,82 +130,16 @@ class RandMoveSIS(RandMove):
             elif S0 <= i < S0+I0:
                 p2.isIncluded = True
                 self.details.addStateChange(i, "I", 0)
+            else:
+                p3.isIncluded=True
+                self.details.addStateChange(i, "R", 0)
             # append them to the data structure
             self.Scollect.append(p1)
             self.Icollect.append(p2)
-
-    # helps _move method with boundary checks
+            self.Rcollect.append(p3)
     
-    # move people within the planSize x planeSize plane
-    def _move(self, day: int, collects: list):
-        """
-        Method that moves each person at the end of each state chage.
 
-        Parameters
-        ----------
-
-        days: int
-            The day that the movement is for. Used to log information in the details class variable. 
-
-        collects: list
-            A list of the different collection data structures in the model. Cycle through the list and change the coordinates to allow for better
-            inheritance. 
-        """
-        # generate the correct number of movement radii
-        movement_r = np.random.normal(self.move_r, self.sigma_R, self.popsize)
-        # generate the random thetas
-        thetas = np.random.uniform(low=0, high=2*math.pi, size=self.popsize)
-        # looping through everyone and moving them; same person coordinates in Scollect and Icoolect,
-        # so which array is looped through doesn't matter
-        for index, person in enumerate(collects[0]):
-            # adjust the x,y coordinate using polar coordinates
-            # conduct the boundary check at the same time
-            x = self._boundaryCheck(person.x + movement_r[index] * math.cos(thetas[index]))
-            y = self._boundaryCheck(person.y + movement_r[index] * math.sin(thetas[index]))
-            # add the new location to the Simul_Details object
-            self.details.addLocation(day, (x,y))
-            # change the x, y coordinates
-            for j, collect in enumerate(collects):
-                collects[j][index].x = x
-                collects[j][index].y = y
-            #self.Scollect[index].x, self.Icollect[index].x = x, x
-            #self.Scollect[index].y, self.Icollect[index].y = y, y
-    
-    # deal with transfers from S to I compartments
-    def _StoI(self, day: int):
-        """
-        Takes care of running state changes from S compartment to I compartment
-
-        Parameters
-        ----------
-
-        day: int
-            The day that the state chagne is for. Used to log information in the details class variable
-        """
-        #print("I've been called!")
-        # set containing the indices for transfers
-        transfers = set()
-        for count, inf in enumerate(self.Icollect):
-            # if the person isn't infected, check the next person
-            if inf.isIncluded == False:
-                continue
-            for index, sus in enumerate(self.Scollect):
-                # if the person isn't in the susceptible bin
-                if sus.isIncluded == False:
-                    continue
-                event = self._infect(inf, sus)
-                # if there is a successful infection
-                if event:
-                    #print("Infection")
-                    # add the index to the transfer set to be transferred later
-                    transfers.add(index)
-                    # change the status to be not included in S collection
-                    self.Scollect[index].isIncluded = False
-                    # adjust the state change in the Simul_Details object
-                    self.details.addTransmission(day, count, index)
-        return transfers
-    
-    def _ItoS(self):
+    def _ItoR(self):
         """Takes care of running state changes from I compartment to S compartment """
         # set that contains the indices for transfering from I to S
         
@@ -222,8 +155,7 @@ class RandMoveSIS(RandMove):
                 transfers.add(index)
                 self.Icollect[index].isIncluded = False
         return transfers
-    
-    # run the simulation
+
     def run(self, getDetails=True):
         """
         Run the actual simulation. 
@@ -243,37 +175,35 @@ class RandMoveSIS(RandMove):
             chains. User can also examine transmission history and state changes of individuals in the object
             by utilizing the Simul_Details object. 
         """
+
         # for all the days in the simulation
         for i in range(1, self.days+1):
             print("Day ", i)
             #print("Location: (", self.Scollect[0].x, ",", self.Scollect[0].y, ").")
             # run the state changes
             StoI = self._StoI(i)
-            ItoS = self._ItoS()
+            ItoR = self._ItoR()
             # change the indices of the transfers
             for index in StoI:
                 self.Icollect[index].isIncluded = True
                 # add state change for each person
                 self.details.addStateChange(index, "I", i)
-            for index in ItoS:
-                self.Scollect[index].isIncluded = True
+            for index in ItoR:
+                self.Rcollect[index].isIncluded = True
                 # add state change for each person
-                self.details.addStateChange(index, "S", i)
+                self.details.addStateChange(index, "R", i)
             # make everyone move randomly
-            self._move(i, [self.Scollect, self.Icollect])
+            self._move(i, [self.Scollect, self.Icollect, self.Rcollect])
             # change the values in the arrays
-            self.S[i] = self.S[i-1] - len(StoI) + len(ItoS)
-            self.I[i] = self.I[i-1] + len(StoI) - len(ItoS)
+            self.S[i] = self.S[i-1] - len(StoI)
+            self.I[i] = self.I[i-1] + len(StoI) - len(ItoR)
+            self.R[i] = self.R[i-1] + len(ItoR)
         if getDetails:
             return self.details
-
-    # switch everything to a dataframe
+    
     def toDataFrame(self):
         """
         Gives user access to pandas dataframe with amount of people in each state on each day.
-
-        Parameters
-        ----------
 
         Returns
         -------
@@ -286,8 +216,8 @@ class RandMoveSIS(RandMove):
         t = np.linspace(0, self.days, self.days + 1)
         # create a 2D array with the days and susceptible and infected arrays
         # do it over axis one so that it creates columns days, susceptible, infected
-        arr = np.stack([t, self.S, self.I], axis=1)
-        df = pd.DataFrame(arr, columns=["Days", "Susceptible", "Infected"])
+        arr = np.stack([t, self.S, self.I, self.R], axis=1)
+        df = pd.DataFrame(arr, columns=["Days", "Susceptible", "Infected", "Removed"])
         return df
     
     # maybe add picking what to plot later
@@ -296,36 +226,16 @@ class RandMoveSIS(RandMove):
         "Plots the number of susceptible and infected individuals on the y-axis and the number of days on the x-axis."
 
         t = np.linspace(0, self.days, self.days + 1)
-        fig, (ax1, ax2) = plt.subplots(nrows=2, sharex='all')
+        fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, sharex='all')
         ax1.plot(t, self.S, label="Susceptible", color='r')
-        ax1.set_ylabel("Number of Susceptible People")
-        ax1.set_title("Random Movement SIS Simulation")
+        ax1.set_ylabel("# Susceptibles")
+        ax1.set_title("Random Movement SIR Simulation")
         ax2.plot(t, self.I, label="Active Cases", color='b')
-        ax2.set_xlabel("Days")
-        ax2.set_ylabel("Active Cases")
+        ax2.set_ylabel("# Active Infections")
+        ax3.set_xlabel("Days")
+        ax3.set_ylabel("# Recovered")
+        ax3.plot(t, self.R, label="Removed")
         ax1.legend()
         ax2.legend()
+        ax3.legend()
         plt.show()
-
-
-                
-
-                
-
-
-
-            
-
-
-    
-
-
-
-
-
-        
-
-
-
-
-
