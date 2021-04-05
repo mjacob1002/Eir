8b3b1d08166c638fb6bc1ce13f573fd9a3749850
Mathew Jacob <mjacob1002@gmail.com>
2021-04-05 12:01:21 -0400

    Tests for randMoveSIS complete.

---
+++ tests/randMove/test_randMoveSIS.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pandas as pd
+import unittest
+
+from src.DTMC.spatialModel.randomMovement.randMoveSIS import RandMoveSIS
+import src.exceptions as e
+
+
+np.random.seed(35235)
+
+class Test_RandMoveSIS(unittest.TestCase):
+
+    def __init__(self):
+        self.test = RandMoveSIS(999, 2, .3, 25, 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.sdetails = self.test.run()
+    
+    def generateCSV(self):
+        df = self.test.toDataFrame()
+        df.to_csv("randMoveSIS.csv", index=False)
+    
+    def checkOutput(self):
+        df = self.test.toDataFrame()
+        df2 = pd.read_csv("randMoveSIS.csv")
+        assert df.equals(df2)
+        print("Output test passed")
+    
+    def checkSimulInputs(self):
+        # checks for invalid person inputs
+        self.assertRaises(e.NotIntException, self.sdetails.personHistory, 100.0)
+        self.assertRaises(e.PersonNotFound, self.sdetails.personHistory, 1001)
+        # checks for exceptions when inputting days
+        self.assertRaises(e.DayOutOfRange, self.sdetails.transmissionHistoryOnDay, 65)
+        self.assertRaises(e.DayOutOfRange, self.sdetails.transmissionHistoryOnDay, -1)
+        self.assertRaises(e.NotIntException, self.sdetails.transmissionHistoryOnDay, 25.0)
+        print("Simul_Details input test passed: throws error for invalid inputs")
+    
+    def checkInputs(self):
+        # int check
+        self.assertRaises(e.NotIntException, RandMoveSIS, 999.0, 1, .3, 25, 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NotIntException, RandMoveSIS, 999, 1.0, .3, 25, 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NotIntException, RandMoveSIS, 999, 1, .3, 25, 3, .3, 1, .25, 31.0, 1.0, 2.0)
+        # float check
+        self.assertRaises(e.NotFloatException, RandMoveSIS, 999, 1, '.3', 25, 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NotFloatException, RandMoveSIS, 999, 1, .3, "25", 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NotFloatException, RandMoveSIS, 999, 1, .3, 25, "3", .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NotFloatException, RandMoveSIS, 999, 1, .3, 25, 3, True, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NotFloatException, RandMoveSIS, 999, 1, .3, 25, 3, .3, 1, .25, 31, False, 2.0)
+        # negvalue check
+        self.assertRaises(e.NegativeValException, RandMoveSIS, -999, 1, .3, 25, 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NegativeValException, RandMoveSIS, 999, -1, .3, 25, 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NegativeValException, RandMoveSIS, 999, 1, -.3, 25, 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NegativeValException, RandMoveSIS, 999, 1, .3, -25, 3, .3, 1, .25, 31, 1.0, 2.0)
+        self.assertRaises(e.NegativeValException, RandMoveSIS, 999, 1, .3, 25, -3, .3, 1, .25, 31, 1.0, 2.0)
+        # probability check
+        self.assertRaises(e.ProbabilityException, RandMoveSIS, 999, 1, .3, 25, 3, .3, 1, .25, 31, 1.01, 2.0)
+        self.assertRaises(e.ProbabilityException, RandMoveSIS, 999, 1, 1.3, 25, 3, .3, 1, .25, 31, 1.0, 2.0)
+
+        print("Input Test passed")
+
+if __name__ == '__main__':
+    a = Test_RandMoveSIS()
+    #a.generateCSV()
+    a.checkOutput()
+    a.checkSimulInputs()
+    a.checkInputs()
+
