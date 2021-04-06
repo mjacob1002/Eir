f1d62d59913e71e05cf8c024ed08d8343b8fb79c
Mathew Jacob <mjacob1002@gmail.com>
2021-04-06 14:32:50 -0400

    Init file to make it less troublesome to import relevant classes.

--- Eir/__init__.py
+++ Eir/__init__.py
@@ -1,1 +1,92 @@
-# init file for src
+# init file for Eir package
+
+# periodic mobility imports
+from DTMC.spatialModel.PeriodicMovement.periodicSIS import *
+from DTMC.spatialModel.PeriodicMovement.periodicSIRVS import *
+from DTMC.spatialModel.PeriodicMovement.periodicSIRV import *
+from DTMC.spatialModel.PeriodicMovement.periodicSIRSDV import *
+from DTMC.spatialModel.PeriodicMovement.periodicSIRSD import *
+from DTMC.spatialModel.PeriodicMovement.periodicSIRS import *
+from DTMC.spatialModel.PeriodicMovement.periodicSIRDV import *
+from DTMC.spatialModel.PeriodicMovement.periodicSIRD import *
+from DTMC.spatialModel.PeriodicMovement.periodicSEIRVS import *
+from DTMC.spatialModel.PeriodicMovement.periodicSEIRV import *
+from DTMC.spatialModel.PeriodicMovement.periodicSEIRSDV import *
+from DTMC.spatialModel.PeriodicMovement.periodicSEIRSD import *
+from DTMC.spatialModel.PeriodicMovement.periodicSEIRS import *
+from DTMC.spatialModel.PeriodicMovement.periodicSEIRDV import *
+from DTMC.spatialModel.PeriodicMovement.periodicSIS import *
+from DTMC.spatialModel.PeriodicMovement.periodicSEIRDV import *
+from DTMC.spatialModel.PeriodicMovement.periodicSEIR import *
+from DTMC.spatialModel.PeriodicMovement.periodicICUV import *
+
+# randomMovement imports
+from DTMC.spatialModel.randomMovement.randMoveSIS import *
+from DTMC.spatialModel.randomMovement.randMoveSIRVS import *
+from DTMC.spatialModel.randomMovement.randMoveSIRV import *
+from DTMC.spatialModel.randomMovement.randMoveSIRSDV import *
+from DTMC.spatialModel.randomMovement.randMoveSIRSD import *
+from DTMC.spatialModel.randomMovement.randMoveSIRS import *
+from DTMC.spatialModel.randomMovement.randMoveSIRDV import *
+from DTMC.spatialModel.randomMovement.randMoveSIRD import *
+from DTMC.spatialModel.randomMovement.randMoveSEIRVS import *
+from DTMC.spatialModel.randomMovement.randMoveSEIRV import *
+from DTMC.spatialModel.randomMovement.randMoveSEIRSDV import *
+from DTMC.spatialModel.randomMovement.randMoveSEIRSD import *
+from DTMC.spatialModel.randomMovement.randMoveSEIRS import *
+from DTMC.spatialModel.randomMovement.randMoveSEIRDV import *
+from DTMC.spatialModel.randomMovement.randMoveSIS import *
+from DTMC.spatialModel.randomMovement.randMoveSEIRDV import *
+from DTMC.spatialModel.randomMovement.randMoveSEIR import *
+
+# Hub import
+from DTMC.spatialModel.Hub.HubSIS import *
+from DTMC.spatialModel.Hub.HubSIRVS import *
+from DTMC.spatialModel.Hub.HubSIRV import *
+from DTMC.spatialModel.Hub.HubSIRSDV import *
+from DTMC.spatialModel.Hub.HubSIRSD import *
+from DTMC.spatialModel.Hub.HubSIRS import *
+from DTMC.spatialModel.Hub.HubSIRDV import *
+from DTMC.spatialModel.Hub.HubSIRD import *
+from DTMC.spatialModel.Hub.HubSEIRVS import *
+from DTMC.spatialModel.Hub.HubSEIRV import *
+from DTMC.spatialModel.Hub.HubSEIRSDV import *
+from DTMC.spatialModel.Hub.HubSEIRSD import *
+from DTMC.spatialModel.Hub.HubSEIRS import *
+from DTMC.spatialModel.Hub.HubSEIRDV import *
+from DTMC.spatialModel.Hub.HubSIS import *
+from DTMC.spatialModel.Hub.HubSEIRDV import *
+from DTMC.spatialModel.Hub.HubSEIR import *
+from DTMC.spatialModel.Hub.Hub_ICUV import *
+
+#Strong Infectious
+from DTMC.spatialModel.StrongInfectious.StrongInfSIS import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSIRVS import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSIRV import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSIRSDV import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSIRSD import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSIRS import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSIRDV import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSIRD import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSEIRVS import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSEIRV import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSEIRSDV import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSEIRSD import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSEIRS import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSEIRDV import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSIS import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSEIRDV import *
+from DTMC.spatialModel.StrongInfectious.StrongInfSEIR import *
+from DTMC.spatialModel.StrongInfectious.StrongInf_ICUV import *
+
+#Deterministic
+
+from Deterministic.SEIR import *
+from Deterministic.SIR import *
+from Deterministic.SIRD import *
+from Deterministic.SIRS import *
+from Deterministic.SIRV import *
+from Deterministic.SIS import *
+
+
+
