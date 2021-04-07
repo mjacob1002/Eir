22a364fe2304a58aa45c249bfb688e0d64df60b8
Mathew Jacob <mjacob1002@gmail.com>
2021-04-07 09:58:31 -0400

    Updated setup files.

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import setup, find_packages
+import os.path
+
+# The directory containing this file
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+# The text of the README file
+with open(os.path.join(HERE, "README.md")) as fid:
+    README = fid.read()
 
 setup(
     name='Eir',
-    version='0.0.1',
+    version='0.1.1',
     license='MIT',
     description="Helps model epidemics using spatial models",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url = "https://github.com/mjacob1002/Eir",
     download_url="https://github.com/mjacob1002/Eir/archive/refs/tags/v_001.tar.gz",
     author="Mathew Jacob",
@@ -15,6 +25,6 @@ setup(
         "pandas", 
         "matplotlib", 
         "multipledispatch"
-        ]
-    packages=find_packages(),
+        ],
+    packages=find_packages()
 )
